0b1009e5c24d938f4d3f42b8882a2f6b4ae94e23
ZhouNian <zhounian@ZhouNiandeMacBook-Pro.local>
2018-12-18 14:49:04 +0800

    线程安全set

---
+++ set.go
@@ -0,0 +1,59 @@
+package goset
+
+import "sync"
+
+var Empty = struct{}{}
+
+type Set struct {
+	m map[interface{}]struct{}
+	sync.RWMutex
+}
+
+func New(items ...interface{}) *Set {
+	s := &Set{}
+	s.m = make(map[interface{}]struct{})
+	s.Add(items...)
+	return s
+}
+
+func (this *Set) Add(items ...interface{}) {
+	for _, item := range items {
+		this.Lock()
+		this.m[item] = Empty
+		this.Unlock()
+	}
+}
+
+func (this *Set) Remove(item interface{}) {
+	this.Lock()
+	delete(this.m, item)
+	this.Unlock()
+}
+
+func (this *Set) Size() int {
+	this.RLock()
+	length := len(this.m)
+	this.RUnlock()
+	return length
+}
+
+func (this *Set) Contains(item interface{}) bool {
+	this.RLock()
+	_, ok := this.m[item]
+	this.RUnlock()
+	return ok
+}
+
+func (this *Set) Clear() {
+	this.Lock()
+	this.m = make(map[interface{}]struct{})
+	this.Unlock()
+}
+
+func (this *Set) List(callback func(items interface{})) {
+	this.RLock()
+	for k, _ := range this.m {
+		callback(k)
+	}
+	this.RUnlock()
+}
---
+++ set_test.go
@@ -0,0 +1,59 @@
+package goset
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	s := New("yearm", "male", 21)
+	s.List(func(items interface{}) {
+		fmt.Println(items)
+	})
+}
+
+func TestSet_Add(t *testing.T) {
+	s := New()
+	s.Add("yearm")
+	s.Add("male")
+	s.Add(21)
+	s.List(func(items interface{}) {
+		fmt.Println(items)
+	})
+}
+
+func TestSet_Remove(t *testing.T) {
+	s := New()
+	s.Add("yearm")
+	s.Add("male")
+	s.Add(21)
+	s.Remove(21)
+	s.List(func(items interface{}) {
+		fmt.Println(items)
+	})
+}
+
+func TestSet_Size(t *testing.T) {
+	s := New("yearm", "male", 21)
+	fmt.Println(s.Size())
+}
+
+func TestSet_Contains(t *testing.T) {
+	s := New("yearm", "male", 21)
+	fmt.Println(s.Contains(21))
+}
+
+func TestSet_Clear(t *testing.T) {
+	s := New("yearm", "male", 21)
+	s.Clear()
+	s.List(func(items interface{}) {
+		fmt.Println(items)
+	})
+}
+
+func TestSet_List(t *testing.T) {
+	s := New("yearm", "male", 21)
+	s.List(func(items interface{}) {
+		fmt.Println(items)
+	})
+}
